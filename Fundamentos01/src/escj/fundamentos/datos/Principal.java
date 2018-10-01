52ef7c37820d15e5260e8d4116e96c960bad6918
Alumno <Alumno@AULA29-4>
2018-10-01 11:16:37 +0200

    Fundamentos

---
+++ Fundamentos01/src/escj/fundamentos/datos/Principal.java
@@ -0,0 +1,43 @@
+package escj.fundamentos.datos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+	
+		// para poner un comentario
+		/* para
+		/ comentario
+		en varias lineas */
+		
+		// TIPOS DE VARIABLES
+		
+		// TIPOS ENTEROS
+		byte b; // desde -128 a +127 (8 bits)
+		short sh; // desde -32768 a +32767 (16 bits)
+		int i; // (32 bits)
+		long l; // (64 bits)
+		char c; // de 0 a 65535
+		char ch = 'A';
+		
+		// TIPOS REALES
+		float f = 3.1416F; // (32 bits)
+		double d; // (64 bits)
+		
+		double pi = Math.PI;
+		
+		// TIPO LÓGICO
+		boolean bol = true; // true o false
+		
+		// TIPOS COMPLEJOS
+		Byte by;
+		Integer ii;
+		Short sho;
+		Long ll;
+		Character cha;
+		Boolean bool;
+		
+		// TIPO CADENA
+		String cadena = "LO QUE SEA";
+		System.out.println(cadena);
+	}
+}
