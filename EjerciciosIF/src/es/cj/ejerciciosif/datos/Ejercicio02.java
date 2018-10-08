8ea8ffe296aec38fe6952f1d89f6dee5a8fac807
Alumno <Alumno@AULA29-4>
2018-10-08 10:30:52 +0200

    8/10/2018

--- EjerciciosIF/src/es/cj/ejerciciosif/datos/Ejercicio02.java
+++ EjerciciosIF/src/es/cj/ejerciciosif/datos/Ejercicio02.java
@@ -4,30 +4,32 @@ import java.util.Scanner;
 
 public class Ejercicio02 {
 
-	// Pedir al usuario dia, mes y año y comprobar que la fecha es correcta incluidos bisiestos.
-	
-	private static Scanner sc = new Scanner(System.in);
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// Pedir al usuario día, mes y año y comprobar que la fecha es correcta
+	// incluidos bisiestos
 
-		System.out.println("Introduzca el día");
+	private static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.println("Día: ");
 		int dia = sc.nextInt();
-		System.out.println("Introduzca el mes");
+
+		System.out.println("Mes: ");
 		int mes = sc.nextInt();
-		System.out.println("Introduzca el año");
+
+		System.out.println("Año: ");
 		int anyo = sc.nextInt();
-		
+
 		boolean fechaCorrecta = false;
-		
+
 		if (anyo >= 0) {
-			boolean esBisiesto = ((anyo % 4 == 0 && anyo % 100 !=0) || (anyo % 400 == 0));
-			if (mes >=1 && mes <= 12) {
+			boolean esBisiesto = ((anyo % 4 == 0 && anyo % 100 != 0) 
+					|| (anyo % 400 == 0));
+			if (mes >= 1 && mes <= 12) {
 				switch (mes) {
 				case 2:
-					if (dia >=1 && dia <= 29 && esBisiesto)
+					if (dia >= 1 && dia <= 29 && esBisiesto)
 						fechaCorrecta = true;
-					else if (dia >=1 && dia <= 28 && !esBisiesto)
+					else if (dia >= 1 && dia <= 28 && !esBisiesto)
 						fechaCorrecta = true;
 					break;
 				case 1:
@@ -40,13 +42,25 @@ public class Ejercicio02 {
 					if (dia >= 1 && dia <= 31)
 						fechaCorrecta = true;
 					break;
-					
+				case 4:
+				case 6:
+				case 9:
+				case 11:
+					if (dia >= 1 && dia <= 30)
+						fechaCorrecta = true;
+					break;
 				default:
 					break;
 				}
 			}
+		}
+		
+		if (fechaCorrecta)
+			System.out.println("Fecha correcta: " + dia + "/" + mes + "/" + anyo);
+		else
+			System.out.println("Fecha incorrecta: " + dia + "/" + mes + "/" + anyo);
+
+		sc.close();
 	}
-	
-	if (fechaCorrecta)System.out.println("Fecha correcta:" + );
-}
+
 }
