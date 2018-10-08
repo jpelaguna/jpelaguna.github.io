07fd8bf6591aadfb1c12120c417327e7293b3122
Alumno <Alumno@AULA29-4>
2018-10-08 10:33:51 +0200

    8/10/2018

--- EjerciciosIF/src/es/cj/ejerciciosif/datos/Ejercicio01.java
+++ EjerciciosIF/src/es/cj/ejerciciosif/datos/Ejercicio01.java
@@ -4,30 +4,30 @@ import java.util.Scanner;
 
 public class Ejercicio01 {
 
-	private static Scanner sc = new Scanner(System.in);
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// pedir nota de 0 a 10 y mostrarla como insuficiente, suficiente, bien, notable
+	// y sobresaliente
 
-		// Pedir nota de 0 a 10 y mostrarla como insuficiente, suficiente, bien, notable y sobresaliente.
-		
+	private static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
 		System.out.println("Nota: ");
 		double nota = sc.nextDouble();
-		
+
 		if (nota >= 0 && nota < 5)
 			System.out.println("Insuficiente");
 		else if (nota >= 5 && nota < 6)
 			System.out.println("Suficiente");
-		else if (nota >=7 && nota < 9)
+		else if (nota >= 6 && nota < 7)
+			System.out.println("Bien");
+		else if (nota >= 7 && nota < 9)
 			System.out.println("Notable");
-		else if (nota >=9 && nota <= 10)
+		else if (nota >= 9 && nota <= 10)
 			System.out.println("Sobresaliente");
 		else
 			System.out.println("Valor incorrecto");
-		
-		
-		switch ((int)nota) {
-		
+
+		switch ((int) nota) {
+		case 0:
 		case 1:
 		case 2:
 		case 3:
@@ -39,6 +39,7 @@ public class Ejercicio01 {
 			break;
 		case 6:
 			System.out.println("Bien");
+			break;
 		case 7:
 		case 8:
 			System.out.println("Notable");
@@ -48,6 +49,7 @@ public class Ejercicio01 {
 			System.out.println("Sobresaliente");
 			break;
 		default:
+			System.out.println("Valor incorrecto");
 			break;
 		}
 	}
--- EjerciciosIF/src/es/cj/ejerciciosif/datos/Explicacion.java
+++ EjerciciosIF/src/es/cj/ejerciciosif/datos/Explicacion.java
@@ -7,12 +7,11 @@ public class Explicacion {
 	private static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Pedir dos números y comprobar si son iguales
 		System.out.println("Nº 1: ");
 		int num1 = sc.nextInt();
 		
-		System.out.println("Nº2: ");
+		System.out.println("Nº 2: ");
 		int num2 = sc.nextInt();
 		
 		if (num1 == num2) {
@@ -22,23 +21,20 @@ public class Explicacion {
 		}
 		
 		boolean condicion = num1 == num2;
-		if (condicion == true)
-			System.out.println(num1 + " es igual a " + num2);
-		else
+		if (!condicion)
 			System.out.println(num1 + " no es igual a " + num2);
+		else
+			System.out.println(num1 + " es igual a " + num2);
 		
-		// Pedir un número e indicar si es positivo, negativo o cero.
+		// Pedir un número e indicar si es positivo, negativo o cero
 		if (num1 > 0)
 			System.out.println(num1 + " es positivo");
-		
 		else if (num1 < 0)
 			System.out.println(num1 + " es negativo");
-		
 		else
-			System.out.println(num1 + "es cero");
+			System.out.println(num1 + " es cero");
 		
-		// Pedir dos números y decir si uno es múltiplo del otro
-		
+		// Pedir 2 números y decir si uno es múltiplo del otro
 		if (num1 % num2 == 0)
 			System.out.println(num1 + " es múltiplo de " + num2);
 		else if (num2 % num1 == 0)
@@ -47,7 +43,6 @@ public class Explicacion {
 			System.out.println("No son múltiplos");
 		
 		// Pedir 2 números e indicar cual es el mayor o si son iguales
-		
 		if (num1 > num2)
 			System.out.println(num1 + " es mayor que " + num2);
 		else if (num2 > num1)
@@ -55,28 +50,24 @@ public class Explicacion {
 		else
 			System.out.println(num1 + " es igual que " + num2);
 		
-		
 		// Pedir 3 números y mostrarlos ordenados
-		System.out.println("Nº3: ");
+		System.out.println("Nº 3: ");
 		int num3 = sc.nextInt();
-		
 		if (num1 > num2 && num1 > num3) {
 			if (num2 > num3)
-				System.out.println("Orden " + num1 + ", " + num2 + ", " + num3);
+				System.out.println("Orden: " + num1 + "," + num2 + "," + num3);
 			else
-				System.out.println("Orden: " + num1 + ", " + num3 + ", " + num2);
+				System.out.println("Orden: " + num1 + "," + num3 + "," + num2);
 		} else if (num2 > num1 && num2 > num3) {
-			if(num1 > num3)
-				System.out.println("Orden: " + num2 + ", " + num1 + ", " + num3);
+			if (num1 > num3)
+				System.out.println("Orden: " + num2 + "," + num1 + "," + num3);
 			else
-				System.out.println("Orden: " + num2 + ", " + num3 + ", " + num1);
-			
+				System.out.println("Orden: " + num2 + "," + num3 + "," + num1);
 		} else if (num3 > num1 && num3 > num2) {
 			if (num1 > num2)
-				System.out.println("Orden: " + num3 + ", " + num1 + ", " + num2);
+				System.out.println("Orden: " + num3 + "," + num1 + "," + num2);
 			else
-				System.out.println("Orden: "+ num3 + ", " + num2 + ", " + num1);
-			
+				System.out.println("Orden: " + num3 + "," + num2 + "," + num1);
 		} else
 			System.out.println("Iguales");
 		
